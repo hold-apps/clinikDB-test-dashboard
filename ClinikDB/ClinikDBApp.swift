7330f20882c30f43b9c05925f0d7170dd8a68d25
Firas Amara <firas.amara@tbs.u-tunis.tn>
2025-02-10 00:15:24 +0100

    Initial Commit

---
+++ ClinikDB/ClinikDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinikDBApp.swift
+//  ClinikDB
+//
+//  Created by Hold Apps on 10/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClinikDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClinikDBTests/ClinikDBTests.swift
@@ -0,0 +1,16 @@
+//
+//  ClinikDBTests.swift
+//  ClinikDBTests
+//
+//  Created by Hold Apps on 10/2/2025.
+//
+
+import Testing
+
+struct ClinikDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
